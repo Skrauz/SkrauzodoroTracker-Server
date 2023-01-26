c74c2b2f3fbce2ebf06e67d50691207802b573d7
FunkySphere <krauzowiczs42@gmail.com>
2023-01-26 16:58:11 +0100

    server setup

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import * as dotenv from "dotenv";
+import cors from "cors";
+import express from "express";
+
+// import functions
+import { connectToDatabase } from "./database";
+
+dotenv.config();
+
+const { SERVER_URI } = process.env;
+const { PORT } = process.env;
+
+if (!SERVER_URI) {
+  console.error(
+    "No SERVER_URI environment variable defined in the config file"
+  );
+  process.exit(1);
+}
+
+connectToDatabase(SERVER_URI)
+  .then(() => {
+    const app = express();
+    app.use(cors());
+
+    app.listen(PORT, () => {
+      console.log(`App running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(`An error has occured: ${error}`);
+  });
