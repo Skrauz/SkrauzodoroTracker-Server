c622aebb7c18b2338a9348f402fd8b9309929411
FunkySphere <krauzowiczs42@gmail.com>
2023-02-18 18:36:10 +0100

    m

--- src/router.ts
+++ src/router.ts
@@ -49,11 +49,30 @@ appRouter.get("/api/projects", async (req, res) => {
   }
 });
 
+appRouter.get("/api/projects/:name?", async (req, res) => {
+  try {
+    const projectName = req?.params?.name;
+    const query = { name: projectName };
+    const project = await collections.projects?.findOne(query);
+
+    if(project) {
+      res.status(200).send(project);
+    } else {
+      res.status(404).send(`Failed to find the project ${projectName}`);
+    }
+  } catch (error: any) {
+    res.status(500).send(error.message)
+  }
+})
+
+// Create Projects
 appRouter.post("/api/projects", async (req, res) => {
     try {
       const project = req.body;
-      console.log(`Creating new project ${project.name}...`);
       if (collections.projects) {
+        if(await hasDuplicates(project)) {
+          res.status(400).send(`Project ${project.name} must have unique name`);
+        }
         const result = await collections.projects.insertOne(project);
         if (result.acknowledged) {
           res
@@ -82,3 +101,36 @@ async function hasDuplicates(project: Project): Promise<boolean> {
   }
   return hasDuplicates;
 }
+
+// Update Project
+appRouter.put("api/projects/:name?", async (req, res) => {
+  try {
+    const projectName = req?.params?.name;
+    const project: Project = req.body;
+
+    const query$ = { name: projectName };
+    const project$ = await collections.projects?.findOne(query$);
+    const query = { _id: new mongodb.ObjectId(project$?._id) };
+
+    const result = await collections.projects?.updateOne(query, {
+      $set: {_id: new mongodb.ObjectId(query._id)},
+    });
+
+    if(result && result.matchedCount) {
+      res.status(200).send(`Updated the project: Name: ${projectName}`);
+      return;
+    }
+
+    if(!result?.matchedCount) {
+      res.status(404).send(`Could not find the project: Name: ${projectName}`);
+      return;
+    }
+
+    res.status(304).send(`Failed to update the project: Name: ${projectName}`);
+
+  }
+  catch (error: any) {
+    console.error(error);
+    res.status(400).send(error.message);
+  }
+})
